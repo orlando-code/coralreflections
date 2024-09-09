9f693edbb0eac24c01e0e188b0967a7c8a840453
Orlando Timmerman <rt582@cam.ac.uk>
2024-09-09 09:56:13 -0300

    added config dataclass

--- reflectance/optimisation_pipeline.py
+++ reflectance/optimisation_pipeline.py
@@ -1,35 +1,47 @@
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler
+from dataclasses import dataclass
 
-def Class():
+
+# custom
+from reflectance import spectrum_utils, file_ops
+
+def OptPipe():
     """
-    Class to run the optimisation pipeline
+    Class to run the optimisation pipeline for a range of parameterisation schema
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, config: OptPipeConfig):
         """Initialises class attributes"""
-        self.spectrum = kwargs
-
+        self.raw_spectra = None
 
     def preprocess_spectrum(self):
         """Correct for glint, calculate subsurface reflectance, crop to sensor range"""
-        continue
+        # fetch indices within NIR wavelength range
+        glint_inds = (self.raw_spectra.columns > min(self.nir_wavelengths)) & (self.raw_spectra.columns < max(self.nir_wavelengths))
+        spectra_deglinted = self.raw_spectra.subtract(self.raw_spectra.loc[:, glint_inds].mean(axis=1), axis=0)
+        # subsurface reflectance
+        spectra_subsurface = spectra_deglinted / (0.518 + 1.562 * spectra_deglinted)
+        # crop to sensor range
+        crop_inds = (spectra_corrected.columns > min(self.sensor_range)) & (spectra_corrected.columns < max(self.sensor_range))
+        # return corrected and cropped spectra for investigation
+        self.spectra = spectra_subsurface.loc[:, crop_inds]
         
     def normalise_spectrum(self):
         """Normalises the spectrum using specified scaler"""
         scaler = self.instantiate_scaler()
-        self.spectrum = scaler.fit_transform
+        self.norm_spectra = scaler.fit_transform(self.spectra)
         # Normalise the spectrum
-        continue
+        pass
     
     def instantiate_scaler(self):
         """Instantiate scaler"""
-        if self.scaler is "zscore":
+        if self.scaler_type == "zscore":
             self.scaler = StandardScaler()
-        elif self.scaler is "minmax":
+        elif self.scaler_type == "minmax":
             self.scaler = MinMaxScaler()
-        elif self.scaler is "robust":
+        elif self.scaler_type == "robust":
             self.scaler = RobustScaler()
-        elif self.scaler is "maxabs":
+        elif self.scaler_type == "maxabs":
             self.scaler = MaxAbsScaler()
         else:
             raise ValueError("Scaler not recognised")
@@ -38,15 +50,15 @@ def Class():
     
     def load_aop_model(self):
         """Load the AOP model dependent on the specified group"""
-        continue
+        self.aop_model = spectrum_utils.read_aop_model(self.aop_group_num)
 
     def do_pca(self):
         """Return the specified number of principal components"""
-        continue
+        pass
     
     def generate_endmembers(self):
         """Generate endmembers using specified method"""
-        continue
+        pass
             
     def run(self):
         """
@@ -64,4 +76,20 @@ def Class():
         # calculate resulting spectra
         # save resulting spectra
         
-        continue
+        pass
+    
+
+@dataclass
+class OptPipeConfig:
+    aop_group_num: int
+    scaler_type: str
+    nir_wavelengths: tuple
+    sensor_range: tuple
+    
+    def __init__(self, conf: dict):
+        self.aop_group_num = conf["aop_group_num"]
+        self.scaler_type = conf["scaler_type"]
+        self.nir_wavelengths = conf["nir_wavelengths"]
+        self.sensor_range = conf["sensor_range"]
+    
+    
