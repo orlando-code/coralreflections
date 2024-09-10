93bac8498944c040d6f98f0a7965c47e073668a3
Orlando Timmerman <rt582@cam.ac.uk>
2024-09-10 12:02:19 -0300

    directory/static path handling

--- reflectance/file_ops.py
+++ reflectance/file_ops.py
@@ -1,6 +1,21 @@
 import yaml
 from pathlib import Path
 
+DATA_DIR_FP = Path(__file__).resolve().parent / 'data'
+RESULTS_DIR_FP = Path(__file__).resolve().parent / 'results'
+
+def get_dir(dir_fp: str | Path) -> Path:
+    dir_fp = Path(dir_fp)
+    if not dir_fp.exists():
+        dir_fp.mkdir()
+    return dir_fp
+
+def get_f(fp: str | Path) -> Path:
+    file_path = Path(fp).resolve()
+    if not file_path.exists():
+        file_path.touch()
+    return file_path
+
 
 def read_yaml(yaml_path: str | Path):
     with open(yaml_path, "r") as file:
