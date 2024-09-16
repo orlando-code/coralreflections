9f3928e8fc87f899eee35b86a9944156b46161cd
Orlando Timmerman <rt582@cam.ac.uk>
2024-09-16 17:47:19 +0000

    black formatting and minor edits to avoid circular dependencies

--- reflectance/file_ops.py
+++ reflectance/file_ops.py
@@ -1,18 +1,19 @@
 # general
 import yaml
 from pathlib import Path
+from itertools import product
 
 # custom
-from reflectance.optimisation_pipeline import GlobalOptPipeConfig, RunOptPipeConfig
+from reflectance import optimisation_pipeline as optpip
 
 # define immutable path structure
 BASE_DIR_FP = Path(__file__).resolve().parent.parent
-MODULE_DIR_FP = BASE_DIR_FP / 'reflectance'
-RESOURCES_DIR_FP = MODULE_DIR_FP / 'resources'
-DATA_DIR_FP = BASE_DIR_FP / 'data'
-RESULTS_DIR_FP = BASE_DIR_FP / 'results'
-TMP_DIR_FP = BASE_DIR_FP / 'tmp'
-CONFIG_DIR_FP = BASE_DIR_FP / 'configs'
+MODULE_DIR_FP = BASE_DIR_FP / "reflectance"
+RESOURCES_DIR_FP = MODULE_DIR_FP / "resources"
+DATA_DIR_FP = BASE_DIR_FP / "data"
+RESULTS_DIR_FP = BASE_DIR_FP / "results"
+TMP_DIR_FP = BASE_DIR_FP / "tmp"
+CONFIG_DIR_FP = BASE_DIR_FP / "configs"
 
 
 def get_dir(dir_fp: str | Path) -> Path:
@@ -34,21 +35,23 @@ def resolve_paths(config, base_dir):
     Resolve relative paths in the configuration to absolute paths.
     """
     for key, value in config.items():
-        if isinstance(value, str) and (value.startswith('data/') or value.startswith('reflectance/')):
+        if isinstance(value, str) and (
+            value.startswith("data/") or value.startswith("reflectance/")
+        ):
             config[key] = (base_dir / value).resolve()
     return config
 
 
 def instantiate_single_configs_instance(run_ind: int = 0):
-    run_cfgs = read_yaml(CONFIG_DIR_FP / 'run_cfgs.yaml')
-    glob_cfg = read_yaml(CONFIG_DIR_FP / 'glob_cfg.yaml')
+    run_cfgs = read_yaml(CONFIG_DIR_FP / "run_cfgs.yaml")
+    glob_cfg = read_yaml(CONFIG_DIR_FP / "glob_cfg.yaml")
     # resolve relative paths
-    glob_cfg = GlobalOptPipeConfig(resolve_paths(glob_cfg, BASE_DIR_FP))
+    glob_cfg = optpip.GlobalOptPipeConfig(resolve_paths(glob_cfg, BASE_DIR_FP))
     # select run configuration
-    run_cfgs = RunOptPipeConfig(run_cfgs[run_ind])
-    
-    return glob_cfg, run_cfgs    
-    
+    run_cfgs = optpip.RunOptPipeConfig(run_cfgs[run_ind])
+
+    return glob_cfg, run_cfgs
+
 
 def read_yaml(yaml_path: str | Path):
     with open(yaml_path, "r") as file:
@@ -65,4 +68,55 @@ def edit_yaml(yaml_path: str | Path, info: dict):
 def save_yaml(yaml_path: str | Path, info: dict):
     with open(yaml_path, "w") as file:
         yaml.dump(info, file)
-        
+
+
+def generate_config_dicts(nested_dict):
+    """
+    Generate a list of configuration dictionaries from a nested dictionary of lists of parameters.
+    """
+
+    def recursive_product(d):
+        if isinstance(d, dict):
+            keys, values = zip(*d.items())
+            if all(isinstance(v, list) for v in values):
+                for combination in product(*values):
+                    yield dict(zip(keys, combination))
+            else:
+                for combination in product(
+                    *[
+                        recursive_product(v) if isinstance(v, dict) else [(k, v)]
+                        for k, v in d.items()
+                    ]
+                ):
+                    yield {k: v for d in combination for k, v in d.items()}
+        else:
+            yield d
+
+    def combine_dicts(dicts):
+        combined = {}
+        for d in dicts:
+            for k, v in d.items():
+                if k not in combined:
+                    combined[k] = v
+                else:
+                    if isinstance(combined[k], dict) and isinstance(v, dict):
+                        combined[k] = combine_dicts([combined[k], v])
+                    else:
+                        combined[k] = v
+        return combined
+
+    # Generate all combinations for the nested dictionaries
+    nested_combinations = []
+    for key, value in nested_dict.items():
+        if isinstance(value, dict):
+            nested_combinations.append([{key: v} for v in recursive_product(value)])
+        else:
+            nested_combinations.append([{key: value}])
+
+    # Combine the nested combinations to preserve the structure
+    final_config_dicts = []
+    for combination in product(*nested_combinations):
+        combined_dict = combine_dicts(combination)
+        final_config_dicts.append(combined_dict)
+
+    return final_config_dicts
--- reflectance/optimisation_pipeline.py
+++ reflectance/optimisation_pipeline.py
@@ -2,10 +2,10 @@
 from dataclasses import dataclass
 from tqdm.auto import tqdm
 import pandas as pd
-import numpy as np
+# import numpy as np
 import logging
 import traceback
-from pathlib import Path
+# from pathlib import Path
 
 # fitting
 import multiprocess as mp
@@ -13,6 +13,7 @@ from functools import partial
 
 # custom
 from reflectance import spectrum_utils, file_ops
+
 
 @dataclass
 class GlobalOptPipeConfig:
@@ -22,7 +23,7 @@ class GlobalOptPipeConfig:
     save_fits: bool
     endmember_map: dict
     endmember_schema: dict
-    
+
     def __init__(self, conf: dict):
         self.spectra_fp = conf["spectra_fp"]
         self.spectral_library_fp = conf["spectral_library_fp"]
@@ -30,7 +31,7 @@ class GlobalOptPipeConfig:
         self.save_fits = conf["save_fits"]
         self.endmember_map = conf["endmember_map"]
         self.endmember_schema = conf["endmember_schema"]
-        
+
 
 @dataclass
 class RunOptPipeConfig:
@@ -47,7 +48,7 @@ class RunOptPipeConfig:
     H_bounds: tuple[float]
     solver: str
     tol: float
-    
+
     def __init__(self, conf: dict):
         self.aop_group_num = conf["processing"]["aop_group_num"]
         self.nir_wavelengths = conf["processing"]["nir_wavelengths"]
@@ -62,8 +63,8 @@ class RunOptPipeConfig:
         self.H_bounds = conf["fitting"]["H_bounds"]
         self.solver = conf["fitting"]["solver"]
         self.tol = conf["fitting"]["tol"]
-    
-    
+
+
 class OptPipe():
     """
     Class to run the optimisation pipeline for a range of parameterisation schema
@@ -82,30 +83,31 @@ class OptPipe():
         # subsurface reflectance
         spectra_subsurface = spectrum_utils.retrieve_subsurface_reflectance(spectra_deglinted)
         # crop to sensor range (with # TEMP RESTRICTION FOR TESTING)
-        self.spectra = spectrum_utils.crop_spectra_to_range(spectra_subsurface, self.cfg.sensor_range).loc[:10,:]
-        # normalise spectra if specified        
+        self.spectra = spectrum_utils.crop_spectra_to_range(spectra_subsurface, self.cfg.sensor_range).loc[:10, :]
+        # normalise spectra if specified
         if self.cfg.spectra_normalisation:
             self.spectra = spectrum_utils.normalise_spectra(self.spectra, self.cfg.spectra_normalisation)
 
     def preprocess_spectral_library(self):
         """Go from raw spectral library to analysis-ready endmembers i.e. crop to relevant wavelengths"""
         self.spectral_library = spectrum_utils.crop_spectra_to_range(self.spectral_library, self.cfg.sensor_range)
-    
-    def convert_classes(self):
-        """Map validation data classes to endmember classes"""
-        endmember_map = self.gcfg.endmember_map
-        mapped_df = pd.DataFrame(index=target_df.index, columns=endmember_map)
 
-        for end_member_type, validation_fields in endmember_map.items():
-            # fill in validation data with sum of all fields in the category
-            mapped_df.loc[:, end_member_type] = target_df.loc[:, validation_fields].sum(axis=1)
-                
-        self.validation_df = mapped_df
-        
+    # def convert_classes(self):
+    #     """Map validation data classes to endmember classes"""
+    #     # TODO: implement
+    #     endmember_map = self.gcfg.endmember_map
+    #     mapped_df = pd.DataFrame(index=target_df.index, columns=endmember_map)
+
+    #     for end_member_type, validation_fields in endmember_map.items():
+    #         # fill in validation data with sum of all fields in the category
+    #         mapped_df.loc[:, end_member_type] = target_df.loc[:, validation_fields].sum(axis=1)
+
+    #     self.validation_df = mapped_df
+
     def retrieve_class_map(self):
         dict_object = next((d for d in self.gcfg.endmember_schema if self.cfg.endmember_class_schema in d), None)
         return list(dict_object.values())[0]
-    
+
     def characterise_endmembers(self):
         """Characterise endmembers using specified method"""
         # remap classes if necessary
@@ -131,19 +133,19 @@ class OptPipe():
     def load_spectra(self):
         """Load spectra"""
         self.raw_spectra = spectrum_utils.load_spectra(self.gcfg.spectra_fp)
-    
+
     def load_aop_model(self):
         """Load the AOP model dependent on the specified group"""
         self.aop_model = spectrum_utils.load_aop_model(self.cfg.aop_group_num)
         aop_sub = self.aop_model.loc[self.spectra.columns]
         self.aop_args = (aop_sub.bb_m.values, aop_sub.bb_c.values, aop_sub.Kd_m.values, aop_sub.Kd_c.values)
-        
+
     def load_spectral_library(self):
         """Load spectral library"""
         self.spectral_library = spectrum_utils.load_spectral_library(self.gcfg.spectral_library_fp)
-            
+
     def return_objective_fn(self):
-        
+
         match self.cfg.objective_fn.lower():
             case "r2":
                 return spectrum_utils.r2_objective_fn
@@ -165,14 +167,14 @@ class OptPipe():
                 return spectrum_utils.spectral_angle_objective_fn_w1
             case _:
                 raise ValueError(f"Objective function {self.cfg.objective_fn} not recognised")
-        
+
     def fit_spectra(self):
         # create wrapper for function to allow parallel processing
         of = self.return_objective_fn()
         partial_wrapper = partial(
-            spectrum_utils._wrapper, 
-            of=of, 
-            prism_spectra=self.spectra, 
+            spectrum_utils._wrapper,
+            of=of,
+            prism_spectra=self.spectra,
             AOP_args=self.aop_args,
             endmember_array=self.endmembers.values,
             Rb_init=0.0001,
@@ -182,28 +184,31 @@ class OptPipe():
             method=self.cfg.solver,
             tol=self.cfg.tol
             )
-        
+
         with mp.Pool() as pool:
             fitted_params = list(tqdm(pool.imap(partial_wrapper, self.spectra.index), total=len(self.spectra.index)))
-        self.fitted_params = pd.DataFrame(fitted_params, index=self.spectra.index, columns=['bb', 'K', 'H'] + list(list(self.endmembers.index)))
-   
-    def calculate_error_metrics(self):
-        """Reconstructing spectra from the fitted parameters"""
-        self.metrics = spectrum_utils.calculate_metrics(self.spectra, self.fitted_params)
-        
+        self.fitted_params = pd.DataFrame(
+            fitted_params, index=self.spectra.index, columns=['bb', 'K', 'H'] + list(list(self.endmembers.index)))
+
+    # def calculate_error_metrics(self):
+    #     """Reconstructing spectra from the fitted parameters"""
+    #     self.metrics = spectrum_utils.calculate_metrics(self.spectra, self.fitted_params)
+
     def generate_results_df(self):
         """Generate a dataframe with a multiindex: run parameters, true spectra, fit results, error metrics"""
         self.results = spectrum_utils.generate_results_df(self.cfg, self.spectra, self.fitted_params, self.metrics)
-        
+
     def generate_spectra_from_fits(self):
-        self.fitted_spectra = spectrum_utils.generate_spectra_from_fits(self.fitted_params, self.spectra.columns, self.endmembers.values, self.aop_args)
-    
+        self.fitted_spectra = spectrum_utils.generate_spectra_from_fits(
+            self.fitted_params, self.spectra.columns, self.endmembers.values, self.aop_args)
+
     def calculate_error_metrics(self):
         self.error_metrics = spectrum_utils.calculate_metrics(self.spectra, self.fitted_spectra)
-        
+
     def generate_fit_results(self):
         # combine fitted_params with fitted_spectra and metrics
-        self.fit_results = spectrum_utils.generate_fit_results(self.fitted_params, self.fitted_spectra, self.error_metrics)
+        self.fit_results = spectrum_utils.generate_fit_results(
+            self.fitted_params, self.fitted_spectra, self.error_metrics)
         # generate filename from summary_results index
         fits_dir_fp = file_ops.get_dir(file_ops.RESULTS_DIR_FP / "fits")
         # find maximum index in results_summary and create fp
@@ -212,12 +217,12 @@ class OptPipe():
         if self.gcfg.save_fits:
             # save to csv if specified
             self.fit_results.to_csv(fits_fp, index=False)
-            
+
     def generate_results_summary(self):
         # TODO: arrangement of method?
         # calculate results summary
         metrics_summary = spectrum_utils.generate_results_summary(self.error_metrics)
-        
+
         # generate metadata
         self.generate_run_metadata()
         self.generate_glob_summary()
@@ -226,24 +231,25 @@ class OptPipe():
             self.generate_error_summary()
         else:
             self.error_df = None
-        # concatenate run summaries        
-        self.results_summary = pd.concat([self.metadata_df, self.glob_summary, self.cfg_df, metrics_summary, self.error_df], axis=1)
+        # concatenate run summaries
+        self.results_summary = pd.concat(
+            [self.metadata_df, self.glob_summary, self.cfg_df, metrics_summary, self.error_df], axis=1)
         self.save_results_summary()
-    
+
     def generate_cfg_summary(self):
         cfg_df = pd.DataFrame([self.cfg.__dict__])
         # create dataframe with run parameters and summary metrics
         multiindex_columns = pd.MultiIndex.from_product([['configuration'], cfg_df.columns])
         cfg_df.columns = multiindex_columns
         self.cfg_df = cfg_df
-        
+
     def generate_run_metadata(self):
         # return date and time as multiindex df with metadata on first, header on second
         datetime = pd.Timestamp.now('UTC')
         # overkill for now, but may add further metadata in future
         multiindex = pd.MultiIndex.from_product([['metadata'], ['datetime (UTC)']])
         self.metadata_df = pd.DataFrame([[datetime]], columns=multiindex)
-        
+
     def generate_glob_summary(self):
         # select keys of glob_cfg to include in summary
         glob_summary = {k: v for k, v in self.gcfg.__dict__.items() if k not in ["endmember_map", "endmember_schema"]}
@@ -253,7 +259,7 @@ class OptPipe():
         multiindex_columns = pd.MultiIndex.from_product([['global_configuration'], glob_summary_df.columns])
         glob_summary_df.columns = multiindex_columns
         self.glob_summary = glob_summary_df
-        
+
     def get_run_id(self):
         # get run id (maximum index in results_summary.csv)
         try:
@@ -273,16 +279,16 @@ class OptPipe():
         else:
             # append results_summary to results_csv file
             self.results_summary.to_csv(results_fp, mode='a', header=False, index=False)
-    
+
     def generate_error_summary(self):
         error_message = f"Error in {self.e_step}: {str(self.e)}\n{traceback.format_exc()}"
         logging.error(error_message)
         self.get_run_id()
         # Append the error message to the results summary
-        error_df = pd.DataFrame([[self.run_id, self.e_step, error_message]], columns=['run_id', 'step', 'error_message'])
+        error_df = pd.DataFrame(
+            [[self.run_id, self.e_step, error_message]], columns=['run_id', 'step', 'error_message'])
         error_df.columns = pd.MultiIndex.from_product([['error'], error_df.columns])
         self.error_df = error_df
-        
 
     def run(self):
         """
@@ -301,7 +307,7 @@ class OptPipe():
         ]
         # # execute pipeline and catch errors
         # for step_name, step_method in pipeline_steps:
-        
+
         #     step_method()
         #     print('step_name', step_name) # useful for debugging since logging otherwise hides until end
 
@@ -314,8 +320,8 @@ class OptPipe():
             try:
                 step_method()
             except Exception as e:
-                print('e', e) # useful for debugging since logging otherwise hides until end
-                print('step_name', step_name) # useful for debugging since logging otherwise hides until end
+                print('e', e)   # useful for debugging since logging otherwise hides until end
+                print('step_name', step_name)   # useful for debugging since logging otherwise hides until end
                 self.e = e
                 self.e_step = step_name
         try:
@@ -327,13 +333,12 @@ class OptPipe():
         except Exception as e:
             print('e', e)
             print(self.cfg)
-        
+
 
 def run_pipeline(glob_cfg: dict, run_cfgs: dict):
     """
     Run the optimisation pipeline for a range of parameterisation schema
     """
-    results = []
     # resolve relative paths in the configuration to absolute paths
     glob_cfg = file_ops.resolve_paths(glob_cfg, file_ops.BASE_DIR_FP)
     glob_cfg = GlobalOptPipeConfig(glob_cfg)
@@ -348,33 +353,19 @@ if __name__ == "__main__":
     # load config_dict
     config_dict = file_ops.read_yaml(file_ops.CONFIG_FP)
     run_pipeline(config_dict)
-    
-    
-    
 
-#### DEPRECATED ####
- 
+
+"""DEPRECATED
+
     # def generate_spectra_from_endmembers(self):
     #     for i, row in tqdm(self.fitted_params.iterrows(), total=len(self.fitted_params)):
     #         bb_m, bb_c, Kd_m, Kd_c = self.aop_args
     #         bb, K, H = row.values[:3]
-    #         pred = spectrum_utils.sub_surface_reflectance_Rb(prism_spectra.columns, endmember_array, bb, K, H, AOP_args, *row.values[3:-2])
+    #         pred = spectrum_utils.sub_surface_reflectance_Rb(
+        # prism_spectra.columns, endmember_array, bb, K, H, AOP_args, *row.values[3:-2])
 
-## been replaced by case switching
-        # if self.cfg.endmember_type == "mean":
-        #     self.endmembers = spectrum_utils.mean_endmembers(self.spectral_library)
-        # elif self.cfg.endmember_type == "median":
-        #     self.endmembers = spectrum_utils.median_endmembers(self.spectral_library)
-        # elif self.cfg.endmember_type in ["pca", "nmf", "ica", "svd"]:
-        #     self.endmembers = spectrum_utils.calculate_endmembers(self.spectral_library, self.cfg.endmember_type)
-        # else:
-        #     raise ValueError(f"Endmember type {self.cfg.endmember_type} not recognised")
-
-## been replaced by error-protected pipeline
+# been replaced by error-protected pipeline
     # def run(self):
-    #     """
-    #     Runs the optimisation pipeline
-    #     """
     #     # load spectra
     #     self.load_spectra()
     #     # preprocess observed spectra, normalising if specified
@@ -397,4 +388,4 @@ if __name__ == "__main__":
     #     self.generate_results_summary()
     #     # calculate and save fits
     #     self.generate_fit_results()
-        
+"""
