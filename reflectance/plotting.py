a909b17a9f7b8ce83a5467b3fc478b90b108416a
Orlando Timmerman <rt582@cam.ac.uk>
2024-10-04 21:00:45 -0300

    ported functions from reeftruth

--- reflectance/plotting.py
+++ reflectance/plotting.py
@@ -472,7 +472,7 @@ def plot_regression_axis(
     metadata: pd.DataFrame = None,
     color_by: str = "Depth",
 ):
-    max_val = max(np.max(test_data), np.max(pred_data))
+
     if metadata is not None:
         color_map = plt.cm.get_cmap("viridis_r" if color_by == "Depth" else "tab20")
         if color_by == "Locale":
@@ -541,14 +541,12 @@ def plot_regression_axis(
     ax.axis("square")
     ax.set_xticks(np.arange(0, 1.1, 0.5))
     ax.set_yticks(np.arange(0, 1.1, 0.5))
-    ax.set_xlim(0, max_val)
-    ax.set_ylim(0, max_val)
     ax.tick_params(axis="both", which="major", labelsize=6)
     ax.grid(True, which="both", ls="-", alpha=0.3)
-    ax.plot([0, max_val], [0, max_val], color="k", ls="--", alpha=0.5)
+    ax.plot([0, 1], [0, 1], color="k", ls="--", alpha=0.5)
 
     if np.sum(pred_data > 0.001):
-        xs = np.linspace(0, max_val, 100)
+        xs = np.linspace(0, 1, 100)
         try:
             p = np.polyfit(test_data.squeeze(), pred_data, 1)
             y_est = np.polyval(p, xs)
@@ -591,6 +589,7 @@ def plot_regression_results(
 
     xs = np.linspace(0, np.max(test_data), 100)
     color_map = plt.cm.get_cmap("viridis_r" if color_by == "Depth" else "tab20")
+    max_val = max(np.max(test_data), np.max(pred_data))
 
     for i, (endmember, ax) in enumerate(
         zip(labels.columns, axs.flat if num_plots > 1 else axs)
@@ -611,6 +610,163 @@ def plot_regression_results(
             metadata,
             color_by,
         )
+        ax.set_xlim(0, max_val)
+        ax.set_ylim(0, max_val)
+
+
+# PORT FROM SHIFTPY/REEFTRUTH
+def get_n_colors_from_hexes(
+    num: int,
+    hex_list: list[str] = ["#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#d83c04"],
+) -> list[str]:
+    """
+    from Wes Anderson: https://github.com/karthik/wesanderson/blob/master/R/colors.R
+    Get a list of n colors from a list of hex codes.
+
+    Args:
+        num (int): The number of colors to return.
+        hex_list (list[str]): The list of hex codes from which to create spectrum for sampling.
+
+    Returns:
+        list[str]: A list of n hex codes.
+    """
+    cmap = get_continuous_cmap(hex_list)
+    colors = [cmap(i / num) for i in range(num)]
+    hex_codes = [mcolors.to_hex(color) for color in colors]
+    return hex_codes
+
+
+class ColourMapGenerator:
+    """
+    Get a colormap for colorbar based on the specified type.
+
+    Parameters
+    ----------
+    cbar_type (str, optional): The type of colormap to retrieve. Options are 'seq' for sequential colormap and 'div'
+        for diverging colormap.
+
+    Returns
+    -------
+    matplotlib.colors.LinearSegmentedColormap: The colormap object.
+    """
+
+    def __init__(self):
+        self.sequential_hexes = ["#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#d83c04"]
+        self.diverging_hexes = ["#3B9AB2", "#78B7C5", "#FFFFFF", "#E1AF00", "#d83c04"]
+        self.cyclical_hexes = [
+            "#3B9AB2",
+            "#78B7C5",
+            "#EBCC2A",
+            "#E1AF00",
+            "#d83c04",
+            "#E1AF00",
+            "#EBCC2A",
+            "#78B7C5",
+            "#3B9AB2",
+        ]
+        self.conf_mat_hexes = ["#EEEEEE", "#3B9AB2", "#cae7ed", "#d83c04", "#E1AF00"]
+        self.residual_hexes = ["#3B9AB2", "#78B7C5", "#fafbfc", "#E1AF00", "#d83c04"]
+        self.lim_red_hexes = ["#EBCC2A", "#E1AF00", "#d83c04"]
+        self.lim_blue_hexes = ["#3B9AB2", "#78B7C5", "#FFFFFF"]
+
+    def get_cmap(self, cbar_type, vmin=None, vmax=None):
+        if cbar_type == "seq":
+            return get_continuous_cmap(self.sequential_hexes)
+        if cbar_type == "inc":
+            return get_continuous_cmap(self.sequential_hexes[2:])
+        elif cbar_type == "div":
+            if not (vmin and vmax):
+                raise ValueError(
+                    "Minimum and maximum values needed for divergent colorbar"
+                )
+            cmap = get_continuous_cmap(self.diverging_hexes)
+            norm = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+            return cmap, norm
+            # return get_continuous_cmap(self.diverging_hexes)
+        elif cbar_type == "res":
+            if not (vmin and vmax):
+                raise ValueError(
+                    "Minimum and maximum values needed for divergent colorbar"
+                )
+            cmap = get_continuous_cmap(self.residual_hexes)
+            norm = mcolors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+            return cmap, norm
+        elif cbar_type == "cyc":
+            return get_continuous_cmap(self.cyclical_hexes)
+        elif cbar_type == "lim_blue":
+            return get_continuous_cmap(self.lim_blue_hexes)
+        elif cbar_type == "lim_red":
+            return get_continuous_cmap(self.lim_red_hexes)
+        elif cbar_type == "spatial_conf_matrix":
+            return mcolors.ListedColormap(self.conf_mat_hexes)
+        else:
+            raise ValueError(f"{cbar_type} not recognised.")
+
+
+def hex_to_rgb(value):
+    """
+    Convert a hexadecimal color code to RGB values.
+
+    Parameters
+    ----------
+    value (str): The hexadecimal color code as a string of 6 characters.
+
+    Returns
+    -------
+    tuple: A tuple of three RGB values.
+    """
+    value = value.strip("#")  # removes hash symbol if present
+    hex_el = len(value)
+    return tuple(
+        int(value[i : i + hex_el // 3], 16)  # noqa
+        for i in range(0, hex_el, hex_el // 3)
+    )
+
+
+def get_continuous_cmap(hex_list, float_list=None):
+    """
+    Create and return a color map that can be used in heat map figures.
+
+    Parameters
+    ----------
+    hex_list (list of str): List of hex code strings representing colors.
+    float_list (list of float, optional): List of floats between 0 and 1, same length as hex_list. Must start with 0
+        and end with 1.
+
+    Returns
+    -------
+    matplotlib.colors.LinearSegmentedColormap: The created color map.
+    """
+    rgb_list = [rgb_to_dec(hex_to_rgb(i)) for i in hex_list]
+    if float_list:
+        pass
+    else:
+        float_list = list(np.linspace(0, 1, len(rgb_list)))
+
+    cdict = dict()
+    for num, col in enumerate(["red", "green", "blue"]):
+        col_list = [
+            [float_list[i], rgb_list[i][num], rgb_list[i][num]]
+            for i in range(len(float_list))
+        ]
+        cdict[col] = col_list
+    cmp = mcolors.LinearSegmentedColormap("my_cmp", segmentdata=cdict, N=256)
+    return cmp
+
+
+def rgb_to_dec(value):
+    """
+    Convert RGB color values to decimal values (each value divided by 256).
+
+    Parameters
+    ----------
+    value (list): A list of three RGB values.
+
+    Returns
+    -------
+    list: A list of three decimal values.
+    """
+    return [v / 256 for v in value]
 
 
 # DEPRECATED
